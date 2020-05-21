5554bd3767f92296b3c515378947517ff7a3e7c0
Rafał Kocoń <226467@student.pwr.edu.pl>
2020-05-21 13:02:13 +0200

    Launcher: changed icon

---
+++ app/src/main/java/com/koconr/smspam/activities/PermissionActivity.java
@@ -0,0 +1,16 @@
+package com.koconr.smspam.activities;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.koconr.smspam.R;
+
+public class PermissionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permission);
+    }
+}
